e036beddf2099f787ea0208ae44ebad5261b72d2
Sangryel Do <sangryel@ab180.co>
2025-06-02 19:15:37 +0900

    replace Test code name

--- ios/exampleUITests/ExampleUITestsLaunchTests.swift
+++ ios/exampleUITests/ExampleUITestsLaunchTests.swift
@@ -7,7 +7,7 @@
 
 import XCTest
 
-final class exampleUITestsLaunchTests: XCTestCase {
+final class ExampleUITestsLaunchTests: XCTestCase {
 
     override class var runsForEachTargetApplicationUIConfiguration: Bool {
         true
